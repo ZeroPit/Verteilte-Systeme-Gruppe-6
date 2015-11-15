485301f058f5dcf4353630fb4187fcfcaa4adb86
ZeroPit <peter.steensen.fl@googlemail.com>
2015-11-15 12:16:51 +0100

    Projekt angelegt

---
+++ VerteilteSysteme/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace VerteilteSysteme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.palOutput = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.palOutput);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(685, 491);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // palOutput
+            // 
+            this.palOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.palOutput.Location = new System.Drawing.Point(6, 19);
+            this.palOutput.Name = "palOutput";
+            this.palOutput.Size = new System.Drawing.Size(673, 466);
+            this.palOutput.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(712, 480);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(220, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(712, 439);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(220, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(944, 515);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel palOutput;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ VerteilteSysteme/Form1.cs
@@ -0,0 +1,155 @@
+﻿using Cloo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VerteilteSysteme
+{
+    public partial class Form1 : Form
+    {
+       
+        double Sx = -2.1;
+        double Sy = -1.3;
+        double Fx = 1;
+        double Fy = 1.3;
+
+        Point pStart = Point.Empty;
+        Point pFinish = Point.Empty;
+
+        Color[] _DrawColors;
+
+        Bitmap _OutputBitmap = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            createColor();
+        }
+
+        private void createColor()
+        {
+            _DrawColors = new Color[256];
+            int i = 0;
+            for (int red = 0; red <= 255; red += 51)
+            {/* the six values of red */
+                for (int green = 0; green <= 255; green += 51)
+                {
+                    for (int blue = 0; blue <= 255; blue += 51)
+                    {
+                        Color lColor = Color.FromArgb(red, green, blue);
+                        _DrawColors[i] = lColor;
+                        ++i;
+                    }
+                }
+            }
+            int i2 = 0;
+            for (; i < 256; i++)
+            {
+                _DrawColors[i] = _DrawColors[i2];
+                i2++;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DrawMandel();
+        }
+
+
+     
+        private void DrawMandel()
+        {
+            Bitmap b = new Bitmap(this.Width, this.Height);
+            double x, y, x1, y1, xx, xmin, xmax, ymin, ymax = 0.0;
+            int looper, s, z = 0;
+            double intigralX, intigralY = 0.0;
+            xmin = Sx;
+            ymin = Sy;
+            xmax = Fx;
+            ymax = Fy;
+            intigralX = (xmax - xmin) / palOutput.Width;
+            intigralY = (ymax - ymin) / palOutput.Height;
+            x = xmin;
+            for (s = 1; s < palOutput.Width; s++)
+            {
+                y = ymin;
+                for (z = 1; z < palOutput.Height; z++)
+                {
+                    x1 = 0;
+                    y1 = 0;
+                    looper = 0;
+                    while (looper < 100 && Math.Sqrt((x1 * x1) + (y1 * y1)) < 2)
+                    {
+                        looper++;
+                        xx = (x1 * x1) - (y1 * y1) + x;
+                        y1 = 2 * x1 * y1 + y;
+                        x1 = xx;
+                    }
+                    double perc = looper / (100.0);
+                    int val = ((int)(perc * 255));
+                    b.SetPixel(s, z, _DrawColors[val]);
+                    y += intigralY;
+                }
+                x += intigralX;
+            }
+            _OutputBitmap = b;
+            palOutput.BackgroundImage = (Image)_OutputBitmap;           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            // pick first platform
+            ComputePlatform platform = ComputePlatform.Platforms[0];
+
+            // create context with all gpu devices
+            ComputeContext context = new ComputeContext(ComputeDeviceTypes.Gpu,
+                new ComputeContextPropertyList(platform), null, IntPtr.Zero);
+
+            // create a command queue with first gpu found
+            ComputeCommandQueue queue = new ComputeCommandQueue(context,
+                context.Devices[0], ComputeCommandQueueFlags.None);
+
+            // load opencl source
+            StreamReader streamReader = new StreamReader("../../kernels.cl");
+            string clSource = streamReader.ReadToEnd();
+            streamReader.Close();
+
+            // create program with opencl source
+            ComputeProgram program = new ComputeProgram(context, clSource);
+
+            // compile opencl source
+            program.Build(null, null, null, IntPtr.Zero);
+
+            // load chosen kernel from program
+            ComputeKernel kernel = program.CreateKernel("helloWorld");
+
+            // create a ten integer array and its length
+            int[] message = new int[] { 1, 2, 3, 4, 5 };
+            int messageSize = message.Length;
+
+            // allocate a memory buffer with the message (the int array)
+            ComputeBuffer<int> messageBuffer = new ComputeBuffer<int>(context,
+                ComputeMemoryFlags.ReadOnly | ComputeMemoryFlags.UseHostPointer, message);
+
+            kernel.SetMemoryArgument(0, messageBuffer); // set the integer array
+            kernel.SetValueArgument(1, messageSize); // set the array size
+
+            // execute kernel
+            queue.ExecuteTask(kernel, null);
+
+            // wait for completion
+            queue.Finish();        
+        }
+
+
+
+    }
+}
